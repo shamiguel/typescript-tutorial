8c3b1a88ba93fc0f3129c001900fb95b72c2d4a3
Shami Tomita <shamito@google.com>
2023-11-07 12:25:55 -0500

    added formsmodule to app.module, forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,22 @@ import { WishItem } from 'src/shared/models/wishItem';
 })
 export class AppComponent {
   items : WishItem[] = [
-    new WishItem("To Learn Angular"), 
+   /*  new WishItem("To Learn Angular"), 
     new WishItem("To Learn TypeScript"),
     new WishItem("To Create a great Angular App!!!"),
-    new WishItem("Get Coffee", true) 
+    new WishItem("Get Coffee", true) */ 
   ]
   title = 'Wish List';
   
   toggleItem(item: WishItem){
     item.isComplete = !item.isComplete
     console.log(item)
+  }
+
+  newWishText = "";
+
+  addNewWish(){
+    this.items.push(new WishItem(this.newWishText))
+    this.newWishText = '';
   }
 }
