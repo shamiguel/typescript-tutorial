668d393c4e77d194a5c0d60e1e2a5e892ddc0d4f
Shami Tomita <shamito@google.com>
2023-11-07 15:44:25 -0500

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
 import { WishItem } from 'src/shared/models/wishItem';
 
+
+const filters = [
+  (item : WishItem) => item, 
+  (item : WishItem) => !item.isComplete,
+  (item : WishItem) => item.isComplete
+]
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   items : WishItem[] = [
-   /*  new WishItem("To Learn Angular"), 
+    new WishItem("To Learn Angular"), 
     new WishItem("To Learn TypeScript"),
     new WishItem("To Create a great Angular App!!!"),
-    new WishItem("Get Coffee", true) */ 
+    new WishItem("Get Coffee", true) 
   ]
+  listFilter : any = '0';
+
+  get visibleItems() : WishItem[] {
+    return this.items.filter(filters[this.listFilter])
+  };
+
   title = 'Wish List';
   
   toggleItem(item: WishItem){
@@ -25,5 +39,10 @@ export class AppComponent {
   addNewWish(){
     this.items.push(new WishItem(this.newWishText))
     this.newWishText = '';
+  }
+
+
+  filterChanged(value:any){
+    console.log(value);
   }
 }
